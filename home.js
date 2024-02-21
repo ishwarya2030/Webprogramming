46e6aa93990d832ae9380d7af5bc989ba9719cb1
ishwarya2030 <ishwaryanarasimman2023@gmail.com>
2024-02-21 15:30:27 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,8 @@
+
+const artistButton = document.getElementById("artistButton");
+const cusButton = document.getElementById("cusButton");
+
+artistButton.addEventListener("click", function() {
+
+    window.location.href ="login.html"; //eplace "artist.html" with the URL of the artist page
+});
